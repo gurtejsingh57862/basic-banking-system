5188ac71860d4e94c0e68f7172c220fd2724cb60
gurtej <gurtejsingh57862@gmail.com>
2021-03-12 20:42:11 +0530

    =init

--- public/js/transfer.js
+++ public/js/transfer.js
@@ -3,9 +3,22 @@ const select = document.querySelector('#selectlist')
 const amount = document.querySelector('#amount')
 const sender_id = document.querySelector('#sender_id')
 const sender_name = document.querySelector('#sender_name')
+const sender_balance = document.querySelector('#sender_balance')
 
 form.addEventListener('submit', (e) => {
     e.preventDefault()
+
+    if (select.options[select.selectedIndex].value == "invalid") {
+        return alert("Please select an Option")
+    }
+
+    if (amount.value <= 0) {
+        return alert("Please enter a valid amount")
+    }
+
+    if (amount.value > sender_balance.innerHTML) {
+        return alert(`${sender_name.innerHTML}: "I don't have that much balance!"`)
+    }
 
     const sender_data = {
         amount: -amount.value
